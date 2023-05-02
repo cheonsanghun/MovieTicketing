e27f9a23bd29f99c3c933ce7975b712574541394
bonggyunjo <kjbg4565388@gmail.com>
2023-05-02 15:36:12 +0900

    state pattern loginframe

--- src/moiveticketing/LoginFrame.java
+++ src/moiveticketing/LoginFrame.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package moiveticketing;
 
 import java.awt.event.ActionEvent;
@@ -19,87 +15,70 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-/**
- *
- * @author Admin
- */
 public class LoginFrame extends JFrame {
 
-     List<LoginDto> companys;
+    List<LoginDto> companys;
     Connection conn;
     PreparedStatement stmt;
     ResultSet rs;
-    
+
     JButton b1, b2;
     JLabel l1, l2;
     JTextField id, pw;
     JPanel p;
     JButton back;
 
-    LoginFrame() {
-        p = new JPanel();
-        setSize(800, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // 화면 가운데 배치
-        setTitle("로그인 화면");
-        setVisible(true);
-        
-        b1 = new JButton("로그인");
-        b1.setBounds(500, 180, 100, 30);
-        b1.setLayout(null);
-        b2 = new JButton("회원가입");
-        b2.setBounds(500, 240, 100, 30);
-        b2.setLayout(null);
+    public interface State {
+        void execute();
+    }
 
-        back = new JButton("뒤로가기");
-        back.setBounds(685, 431, 100, 30);
-        back.setLayout(null);
+    public class LoginSuccessState implements State {
+        @Override
+        public void execute() {
+            JOptionPane.showMessageDialog(null, "로그인 성공 !", "로그인 확인!", JOptionPane.DEFAULT_OPTION);
+        }
+    }
 
-        l1 = new JLabel("아이디 : ");
-        l1.setBounds(200, 180, 100, 30);
-        l1.setLayout(null);
-        l2 = new JLabel("비밀번호 : ");
-        l2.setBounds(200, 240, 100, 30);
-        l2.setLayout(null);
+    public class LoginFailureState implements State {
+        @Override
+        public void execute() {
+            JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호가 틀렸습니다", "로그인 실패", 1);
+        }
+    }
 
-        id = new JTextField();
-        id.setBounds(275, 180, 185, 30);
-        id.setLayout(null);
+    public class LoginErrorState implements State {
+        private String errorMessage;
 
-        pw = new JTextField();
-        pw.setBounds(275, 240, 185, 30);
-        pw.setLayout(null);
+        public LoginErrorState(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
 
-        add(b1);
-        add(b2);
-        add(back);
-        add(l1);
-        add(l2);
-        add(id);
-        add(pw);
-        add(p);
-        b1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-                
-                
-                String uid = id.getText();
-                String upw = pw.getText();
-                 try {
-                     String sql = String.format("select pw from profile where id ='%s' and pw = '%s'",uid,upw);	
-                    final String driver = "org.mariadb.jdbc.Driver";
-		final String DB_IP = "localhost";
-		final String DB_PORT = "3306";
-		final String DB_NAME = "test";
-		final String DB_URL = 
-				"jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
-                    conn= DriverManager.getConnection(DB_URL,"root", "12341234");                   
-                        stmt = conn.prepareStatement(sql);
-                       rs=stmt.executeQuery();     
-                       rs.next();
-                       
-               if(uid.length()==0){
+        @Override
+        public void execute() {
+            JOptionPane.showMessageDialog(null, errorMessage, "로그인 오류", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+    public class LoginStateMachine {
+        private State loginSuccessState = new LoginSuccessState();
+        private State loginFailureState = new LoginFailureState();
+
+        private State state;
+        private String uid;
+        private String upw;
+
+        public LoginStateMachine(String uid, String upw) {
+            this.uid = uid;
+            this.upw = upw;
+        }
+
+        public void setState(State state) {
+            this.state = state;
+        }
+
+        public void login() {
+            try {
+                 if(uid.length()==0){
                      JOptionPane.showMessageDialog(null, "아이디를 입력하셔야 합니다.", "아이디 입력!", JOptionPane.DEFAULT_OPTION);
                     return;
                }  
@@ -111,41 +90,93 @@ public class LoginFrame extends JFrame {
                     JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 입력 하셔야 됩니다.", "아이디나 비번을 입력!", JOptionPane.DEFAULT_OPTION);
                     return;
                 }
-               
-                 
-                    if(pw.equals(rs.getString(1)))
-                         System.out.println("성공 !");
-                        JOptionPane.showMessageDialog(null, "로그인 성공 !","로그인 확인!",JOptionPane.DEFAULT_OPTION);
-                      
-                                    
-                } 
-                catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호가 틀렸습니다", "로그인 실패", 1);
-				System.out.println("SQLException" + ex);
-                }
-                 if(uid.length()==0 && upw.length()==0){
-                    JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 입력하세요.", "로그인 오류", JOptionPane.PLAIN_MESSAGE);
-                }
-                if(uid.length()==0 &&upw.length()!=0){
-                    JOptionPane.showMessageDialog(null, "아이디를 입력하세요", "로그인 오류", JOptionPane.PLAIN_MESSAGE);
-                }
-                  if(uid.length()!=0 &&upw.length()==0){
-                    JOptionPane.showMessageDialog(null, "비밀번호를 입력하세요", "로그인 오류", JOptionPane.PLAIN_MESSAGE);
-                }
-            }
-        });
-        b2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Membership membership = new Membership();
-            }
-        });
 
-        back.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Home home = new Home();
+                final String driver = "org.mariadb.jdbc.Driver";
+                final String DB_IP = "localhost";
+                final String DB_PORT = "3306";
+                final String DB_NAME = "test";
+                final String DB_URL =
+                        "jdbc:mariadb://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
+                conn = DriverManager.getConnection(DB_URL, "root", "12341234");
+
+                String sql = String.format("select pw from profile where id ='%s' and pw = '%s'", uid, upw);
+                stmt = conn.prepareStatement(sql);
+                rs = stmt.executeQuery();
+                rs.next();
+
+                if (upw.equals(rs.getString(1))) {
+                    setState(loginSuccessState);
+                } else {
+                    setState(loginFailureState);
+                }
+
+            } catch (SQLException ex) {
+                setState(loginFailureState);
+                System.out.println("SQLException" + ex);
             }
-        });
+        }
     }
+
+    LoginFrame() {
+    p = new JPanel();
+    setSize(800, 500);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null); // 화면 가운데 배치
+    setTitle("로그인 화면");
+    setVisible(true);
+
+    b1 = new JButton("로그인");
+    b1.setBounds(500, 180, 100, 30);
+    b1.setLayout(null);
+    b1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String uid = id.getText();
+            String upw = pw.getText();
+            LoginStateMachine stateMachine = new LoginStateMachine(uid, upw);
+            stateMachine.login();
+            stateMachine.state.execute();
+        }
+    });
+
+    b2 = new JButton("회원가입");
+    b2.setBounds(500, 240, 100, 30);
+    b2.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            Membership membership = new Membership();
+        }
+    });
+
+    l1 = new JLabel("아이디 : ");
+    l1.setBounds(200, 180, 100, 30);
+
+    l2 = new JLabel("비밀번호 : ");
+    l2.setBounds(200, 240, 100, 30);
+
+    id = new JTextField();
+    id.setBounds(275, 180, 185, 30);
+
+    pw = new JTextField();
+    pw.setBounds(275, 240, 185, 30);
+
+    back = new JButton("돌아가기");
+    back.setBounds(685, 431, 100, 30);
+    back.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Home home = new Home();
+        }
+    });
+
+    add(l1);
+    add(l2);
+    add(id);
+    add(pw);
+    add(b1);
+    add(b2);
+    add(back);
+    add(p);
+}
 }
